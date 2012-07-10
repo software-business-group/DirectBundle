2be7c0d0ee8bc15482e64cc011c8cdd018096236
Semyon Velichko <semyon@velichko.net>
2012-07-10 22:18:10 +0300

    Response::success = null

--- Response/Response.php
+++ Response/Response.php
@@ -19,7 +19,7 @@ class Response implements ResponseInterface
     protected $factroy;
     
     protected $data = array();
-    protected $success = true;
+    protected $success;
     protected $total;
     
     public function setFactory(ResponseFactory $factory)
